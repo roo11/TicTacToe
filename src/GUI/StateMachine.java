a6bf6da1d2b840d2084e658336b887fc73658385
roo11 <robertowen096@gmail.com>
2017-05-13 21:19:51 +0100

    Started on the state machine

---
+++ src/GUI/StateMachine.java
@@ -0,0 +1,40 @@
+package GUI;
+
+/**
+ * Created by Robert on 13/05/2017.
+ */
+
+public class StateMachine {
+
+
+
+    switch case()
+
+    {
+        case 1:
+            WinnerX();
+        case 2:
+            WinnerO();
+        case 3:
+            Draw();
+        case 4:
+            notWonYet();
+    }
+
+    WinnerX() {
+
+
+    }
+
+    WinnerO() {
+
+    }
+
+    Draw() {
+
+    }
+
+    notWonYet() {
+
+    }
+}
