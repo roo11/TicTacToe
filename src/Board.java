187d78db3389eb8728ca5f9f03c8a02f39e32b28
Joshua Hodges <j.b.h.0411@gmail.com>
2017-05-13 21:18:17 +0100

    display window

--- src/Board.java
+++ src/Board.java
@@ -3,12 +3,14 @@
  */
 
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Board extends Application {
 
-    StackPane rootStackPane = new StackPane();
+    StackPane boardStackPane = new StackPane();
+    Scene boardScene = new Scene(boardStackPane);
 
     public static void main(String[] args) {
         launch(args);
@@ -17,6 +19,12 @@ public class Board extends Application {
     @Override
     public void start(Stage primaryStage) {
 
+        //Sets up the board
+        Stage boardStage = new Stage();
+        boardStage.setScene(boardScene);
 
+
+        //Finalising and displaying the board
+        boardStage.show();
     }
 }
