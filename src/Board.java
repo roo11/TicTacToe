76247143809c4b45d443fd9486b41e51e88364c6
Joshua Hodges <j.b.h.0411@gmail.com>
2017-05-13 20:48:48 +0100

    created first file

---
+++ src/Board.java
@@ -0,0 +1,22 @@
+/**
+ * Created by josh on 13/05/17.
+ */
+
+import javafx.application.Application;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Board extends Application {
+
+    StackPane rootStackPane = new StackPane();
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+
+    }
+}
